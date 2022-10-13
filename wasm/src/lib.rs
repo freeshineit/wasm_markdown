897bc29c5a7ce9cf07ad9e4564ebc2c9acb8de0e
ShineShao <xiaoshaoqq@gmail.com>
2022-10-13 10:20:18 +0800

    test

--- wasm/src/lib.rs
+++ wasm/src/lib.rs
@@ -22,9 +22,17 @@ pub fn parser(text: &str) -> String {
 
 #[cfg(test)]
 mod tests {
+
+    use super::*;
+
     #[test]
     fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
+        assert_eq!(
+            parser("> a"),
+            r#"<blockquote>
+<p>a</p>
+</blockquote>
+"#
+        );
     }
 }
